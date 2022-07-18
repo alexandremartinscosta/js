00bc88af87ea53ebd6fc2f757676cbc2cc895859
Alexandre Martins Costa <alexandre.martinscosta@gmail.com>
2022-07-18 16:13:59 -0300

    anotações object

---
+++ js/object.js
@@ -0,0 +1,19 @@
+const pessoa = new Object({
+    nome: 'Alexandre'
+});
+
+console.log(pessoa);
+
+// Para definir propriedades de um objeto de forma imutável,
+// usar o Object.defineProperties()
+const zelda = {};
+
+Object.defineProperties(zelda, {
+    criador: {
+        value: 'Shigeru Miyamoto',
+        configurable: false, // Impede que a propriedade seja deletada
+        writable: true, // Permite alterar o valor da propriedade
+    }
+});
+
+console.log(zelda);
